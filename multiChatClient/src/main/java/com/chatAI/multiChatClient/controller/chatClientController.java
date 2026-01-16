13591163c4538404bab1e18a11db1c4881a28da1
SudeepSubediFuji <subedi.sudeep@fujisystems.co.jp>
2026-01-16 18:14:43 +0900

    promptテンプレートを追加

--- multiChatClient/src/main/java/com/chatAI/multiChatClient/controller/chatClientController.java
+++ multiChatClient/src/main/java/com/chatAI/multiChatClient/controller/chatClientController.java
@@ -2,6 +2,7 @@ package com.chatAI.multiChatClient.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
