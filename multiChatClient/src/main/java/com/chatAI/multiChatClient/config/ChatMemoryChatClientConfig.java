c69dae0793fe3bfd7129dd59c7f62cd1119f86e4
SudeepSubediFuji <subedi.sudeep@fujisystems.co.jp>
2026-01-26 15:31:01 +0900

    chatMemoryはJdbcChatMemoryRepositoryを使う

--- multiChatClient/src/main/java/com/chatAI/multiChatClient/config/ChatMemoryChatClientConfig.java
+++ multiChatClient/src/main/java/com/chatAI/multiChatClient/config/ChatMemoryChatClientConfig.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,15 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ChatMemoryChatClientConfig {
+
+    // Define ChatMemory bean using JdbcChatMemoryRepository
+    // Default to -> JdbcChatMemoryRepository rather than InMemoryChatMemoryRepository for long-term storage
+    // Here chatMemory is configured to keep last 10 messages in memory per conversation
+    // Also name chatMemory will be overriden and add to other below.
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().chatMemoryRepository(jdbcChatMemoryRepository).maxMessages(10).build();
+    }
 
     @Bean("chatMemoryChatClient")
     public ChatClient ChatMemoryChatClient(@Qualifier("ollamaChatMemoryClientBase") ChatClient baseClient, ChatMemory chatMemory) {
