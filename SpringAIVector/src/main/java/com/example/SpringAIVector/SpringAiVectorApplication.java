d23874e7de3db501cba75ee53c19a21b676e6f6e
SudeepSubediFuji <subedi.sudeep@fujisystems.co.jp>
2026-01-27 14:50:52 +0900

    ベクターデータ運用

---
+++ SpringAIVector/src/main/java/com/example/SpringAIVector/SpringAiVectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAIVector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiVectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiVectorApplication.class, args);
+	}
+
+}
---
+++ SpringAIVector/src/test/java/com/example/SpringAIVector/SpringAiVectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAIVector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiVectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
