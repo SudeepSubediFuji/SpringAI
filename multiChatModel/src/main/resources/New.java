8036173ae5f52871de64b5aaa7dfde4df42edb72
SudeepSubediFuji <subedi.sudeep@fujisystems.co.jp>
2026-01-28 16:23:22 +0900

    Git整理

---
+++ multiChatModel/src/main/resources/New.java
@@ -0,0 +1,15 @@
+package com;
+
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New {
+
+    public static void main(String[] args) {
+        SpringApplication.run(New.class, args);
+    }
+
+}
